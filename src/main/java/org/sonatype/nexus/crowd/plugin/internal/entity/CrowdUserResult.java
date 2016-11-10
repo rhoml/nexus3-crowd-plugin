1672d4368f59cc585feaab83d91437e80a87435f
Zhenlei Huang <zlei.huang@gmail.com>
2016-11-10 16:45:12 +0800

    fix parsing CrowdUserResule firstName/lastName

--- src/main/java/org/sonatype/nexus/crowd/plugin/internal/entity/CrowdUserResult.java
+++ src/main/java/org/sonatype/nexus/crowd/plugin/internal/entity/CrowdUserResult.java
@@ -1,41 +1,59 @@
 package org.sonatype.nexus.crowd.plugin.internal.entity;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CrowdUserResult {
 
 	private String name;
+
+	@SerializedName("first-name")
 	private String firstName;
+
+	@SerializedName("last-name")
 	private String lastName;
+
 	private boolean active;
+
 	private String email;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public boolean isActive() {
 		return active;
 	}
+
 	public void setActive(boolean active) {
 		this.active = active;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 }
